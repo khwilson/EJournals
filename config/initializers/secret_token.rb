1291aef8014b49918100eb850476748894adb8c0
Kevin <khwilson@gmail.com>
2012-03-24 14:00:14 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ejournals::Application.config.secret_token = '3c4b49513c7f3b3fc1834363e5aa3b4de7b4c5752695a0d47b80cbbbaf34268a7955393e3b727bcd907088147cf8cc3d2dce7fc5acbd1a8e8a517cd403477022'
